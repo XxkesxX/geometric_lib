08b15ef267a7392bcfaf767ef1620c239a5bbe3a
XxkesxX <89289300458@mail.ru>
2024-10-17 18:47:55 +0300

    added descriptions of functions

--- circle.py
+++ circle.py
@@ -2,9 +2,13 @@ import math
 
 
 def area(r):
+    '''Принимает радиус окружности и возвращает её площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус окружности и возвращает его периметр'''
     return 2 * math.pi * r
 
+print(f"При r = 3 S = {area(3)}")
+print(f"При r = 5 P = {perimeter(5)}")
