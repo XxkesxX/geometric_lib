cbcc26a42595e72fa23609f4956a5ac11b134883
XxkesxX <89289300458@mail.ru>
2024-11-01 18:15:49 +0300

    edit .py files

--- square.py
+++ square.py
@@ -6,6 +6,3 @@ def area(a):
 def perimeter(a):
     '''Принимает сторону квадрата и возвращает его периметр'''
     return 4 * a
-
-print(f"При a = 3 S = {area(3)}")
-print(f"При a = 2 P = {perimeter(2)}")
